e3fca93ff5f9a251c8e8754986a4066e5774c08b
Jenna <tqj5047@gmail.com>
2021-01-25 10:36:32 -0500

    d

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const compression = require('compression')
-const passport = require('passport')
 const PORT = process.env.PORT || 8080
 const app = express()
 const socketio = require('socket.io')
